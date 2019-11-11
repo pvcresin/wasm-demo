4eaa7818bc4131e9de20f8fcfb7ad740b7443343
pvcresin <pvcresin0730@gmail.com>
2019-11-11 23:32:04 +0900

    Show file names

--- src/crate/src/lib.rs
+++ src/crate/src/lib.rs
@@ -1,6 +1,9 @@
 mod utils;
 
+use std::io::Cursor;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsValue;
+use zip::ZipArchive;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +11,24 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn initialize() {
+    utils::set_panic_hook();
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, crate!");
+#[wasm_bindgen(js_name = getFilenameList)]
+pub fn get_filename_list(buf: Vec<u8>) -> Vec<JsValue> {
+    let reader = Cursor::new(buf);
+    let mut zip = ZipArchive::new(reader).unwrap();
+    let mut filename_list: Vec<String> = Vec::new();
+
+    for idx in 0..zip.len() {
+        let file = zip.by_index(idx).unwrap();
+        let name = file.name().to_owned();
+        if !name.ends_with("/") {
+            filename_list.push(name);
+        }
+    }
+
+    filename_list.iter().map(|x| JsValue::from_str(x)).collect()
 }
