22965acd6479557a12c09142f3731057af5e88dc
pvcresin <pvcresin0730@gmail.com>
2019-12-15 17:16:47 +0900

    Extract file

--- src/crate/src/lib.rs
+++ src/crate/src/lib.rs
@@ -16,19 +16,42 @@ pub fn initialize() {
     utils::set_panic_hook();
 }
 
-#[wasm_bindgen(js_name = getFilenameList)]
-pub fn get_filename_list(buf: Vec<u8>) -> Vec<JsValue> {
-    let reader = Cursor::new(buf);
-    let mut zip = ZipArchive::new(reader).unwrap();
-    let mut filename_list: Vec<String> = Vec::new();
+#[wasm_bindgen]
+pub struct ZipReader {
+    zip: ZipArchive<Cursor<Vec<u8>>>,
+}
 
-    for idx in 0..zip.len() {
-        let file = zip.by_index(idx).unwrap();
-        let name = file.name().to_owned();
-        if !name.ends_with("/") {
-            filename_list.push(name);
-        }
+#[wasm_bindgen]
+impl ZipReader {
+    #[wasm_bindgen(constructor)]
+    pub fn new(buf: Vec<u8>) -> ZipReader {
+        let reader = Cursor::new(buf);
+        let zip = ZipArchive::new(reader).unwrap();
+        ZipReader { zip }
     }
 
-    filename_list.iter().map(|x| JsValue::from_str(x)).collect()
+    #[wasm_bindgen(js_name = getFilenameList)]
+    pub fn get_filename_list(&mut self) -> Vec<JsValue> {
+        let zip = &mut self.zip;
+        let mut filename_list: Vec<String> = Vec::new();
+
+        for idx in 0..zip.len() {
+            let file = zip.by_index(idx).unwrap();
+            let name = file.name().to_owned();
+            if !name.ends_with("/") {
+                filename_list.push(name);
+            }
+        }
+
+        filename_list.iter().map(|x| JsValue::from_str(x)).collect()
+    }
+
+    #[wasm_bindgen(js_name = getBinary)]
+    pub fn get_binary(&mut self, filename: &str) -> Vec<u8> {
+        let zip = &mut self.zip;
+        let mut file = zip.by_name(filename).unwrap();
+        let mut buf = Vec::with_capacity(file.size() as usize);
+        std::io::copy(&mut file, &mut buf).unwrap();
+        buf
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import saveAs from 'file-saver';
+
 const $input = document.querySelector('[data-js="input"]');
 const $list = document.querySelector('[data-js="list"]');
 const $template = document.querySelector('[data-js="list-item-template"]');
-
-$input.addEventListener('change', loadZip);
 
 async function readFile(file) {
   const ab = await new Promise((resolve, reject) => {
@@ -12,6 +12,13 @@ async function readFile(file) {
     reader.readAsArrayBuffer(file);
   });
   return new Uint8Array(ab);
+}
+
+function extractFile(zipReader, filename) {
+  const buffer = zipReader.getBinary(filename);
+  const blob = new Blob([buffer], { type: 'application/octet-stream' });
+  const basename = filename.split('/').pop();
+  saveAs(blob, basename);
 }
 
 async function loadZip(ev) {
@@ -24,5 +31,25 @@ async function loadZip(ev) {
   ev.target.toggleAttribute('disabled', true);
 
   const buffer = await readFile(file);
-  console.log(wasm.getFilenameList(buffer));
+  const zipReader = new wasm.ZipReader(buffer);
+  const filenameList = zipReader.getFilenameList();
+
+  console.log(filenameList);
+
+  for (const filename of filenameList) {
+    const $item = document.importNode($template.content, true);
+    const $filename = $item.querySelector('[data-js="filename"]');
+    const $extract = $item.querySelector('[data-js="extract-button"]');
+
+    $filename.textContent = filename;
+    $extract.addEventListener('click', () => {
+      $extract.toggleAttribute('disabled', true);
+      extractFile(zipReader, filename);
+      $extract.toggleAttribute('disabled', false);
+    });
+
+    $list.appendChild($item);
+  }
 }
+
+$input.addEventListener('change', loadZip);
